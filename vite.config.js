5bea99c3d5ce667749bc1bf51058b0a0713683b5
kimMG080624 <mingyeolkim637@gmail.com>
2026-03-10 15:09:15 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+cat > vite.config.js << 'EOF'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/wonju-class-31/',
   build: { outDir: 'dist' }
 })
+EOF
